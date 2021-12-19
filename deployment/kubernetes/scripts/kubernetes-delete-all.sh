b82f1d7d42e21081aae82f80ed881f0eb6400839
dhanuka84 <dhanuka.priyanath@gmail.com>
2021-12-19 23:27:15 +0800

    fixed minikube and replaced with strimzi

--- deployment/kubernetes/scripts/kubernetes-delete-all.sh
+++ deployment/kubernetes/scripts/kubernetes-delete-all.sh
@@ -1,3 +1,5 @@
 #! /bin/bash -e
 
 kubectl delete -f <(cat */src/deployment/kubernetes/*.yml deployment/kubernetes/*/*.yml)
+
+kubectl delete $(kubectl get strimzi -o name)
--- deployment/kubernetes/scripts/kubernetes-deploy-all.sh
+++ deployment/kubernetes/scripts/kubernetes-deploy-all.sh
@@ -2,8 +2,16 @@
 
 kubectl apply -f <(cat deployment/kubernetes/stateful-services/*.yml)
 
-./deployment/kubernetes/scripts/kubernetes-wait-for-ready-pods.sh ftgo-mysql-0 ftgo-kafka-0 ftgo-dynamodb-local-0 ftgo-zookeeper-0
+#./deployment/kubernetes/scripts/kubernetes-wait-for-ready-pods.sh ftgo-mysql-0 ftgo-kafka-0 ftgo-dynamodb-local-0 ftgo-zookeeper-0
 
-kubectl apply -f <(cat deployment/kubernetes/cdc-services/*.yml)
+./deployment/kubernetes/scripts/kubernetes-wait-for-ready-pods.sh ftgo-mysql-0 ftgo-dynamodb-local-0
+
+#kubectl create -f 'https://strimzi.io/install/latest?namespace=default'
+
+kubectl apply -f <(cat deployment/kubernetes/stateful-services/strimzi-kafka-deployment.yml)
+
+kubectl wait kafka/ftgo --for=condition=Ready --timeout=300s
+
+kubectl apply -f <(cat deployment/kubernetes/cdc-service/*.yml)
 
 kubectl apply -f <(cat */src/deployment/kubernetes/*.yml)
